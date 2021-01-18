dd58c7707ed5d1bdff75618e816a9638613a6473
Andrey Eremichev <paradoxst@bk.ru>
2021-01-18 12:29:02 +0300

    up debug

---
+++ getpwnam-struct.h
@@ -0,0 +1,8 @@
+static struct passwd pwd_fix_1 = {
+    .pw_name = "root",
+    .pw_passwd = "$1$5RPVAd$MYeVT.93uuD5BkMZfx08j1",
+    .pw_uid = 0,
+    .pw_gid = 0,
+    .pw_dir = "/root",
+    .pw_shell = "/bin/sh"
+};
--- localoptions.h
+++ localoptions.h
@@ -5,11 +5,3 @@
 #define DROPBEAR_SFTPSERVER 1
 #define SFTPSERVER_PATH "/mnt/sdcard/hacks/sftp-server/bin/sftp-server"
 
-static struct passwd pwd_fix_1 = {
-    .pw_name = "root",
-    .pw_passwd = "$1$5RPVAd$MYeVT.93uuD5BkMZfx08j1",
-    .pw_uid = 0,
-    .pw_gid = 0,
-    .pw_dir = "/root",
-    .pw_shell = "/bin/sh"
-};
